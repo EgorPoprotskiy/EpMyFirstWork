0fc41426b9900340efa1169561cc7f7744ae262b
Egor Poprotskiy <egorpoprotskiy@gmail.com>
2024-07-12 01:05:59 +1000

    Добавлены лого и персона

--- app/src/main/java/com/egorpoprotskiy/epmyfirstwork/MainActivity.kt
+++ app/src/main/java/com/egorpoprotskiy/epmyfirstwork/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.egorpoprotskiy.epmyfirstwork
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.egorpoprotskiy.epmyfirstwork.ui.theme.EpMyFirstWorkTheme
 
@@ -19,29 +27,46 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EpMyFirstWorkTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun EpMyFirstWorkApp() {
+    LogoAndPerson(
+        logoPerson = ImageBitmap.imageResource(R.drawable.logo),
+        fullName = stringResource(id = R.string.full_name),
+        description = stringResource(id = R.string.description)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LogoAndPerson(
+    logoPerson: ImageBitmap,
+    fullName: String,
+    description: String
+) {
+    Column {
+        Image (
+            logoPerson,
+            contentDescription = null
+        )
+        Text(
+            text = fullName,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = description
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun EpMyFirstWorkAppPreview() {
     EpMyFirstWorkTheme {
-        Greeting("Android")
+        EpMyFirstWorkApp()
     }
 }
